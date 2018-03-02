8a7eb4af69cc7f15c7056e46c799d60f53a47e3b
Waleri Tumanez <w.tumanez@rmh.de>
2018-03-02 19:41:32 +0100

    add: babel polyfiller if not exists

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import 'es6-promise/auto'
 
 import dataTable from './src/components/data-table/data-table.vue'
 
+if (!global._babelPolyfill) {
+	require('babel-polyfill')
+}
+
 export {
   dataTable
 }
